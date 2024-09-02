bb1bf7094cfbd74e8f1a78e87a59888c001dbfdc
João Carlos <89033866+Churima@users.noreply.github.com>
2024-09-02 19:31:09 -0300

    Ajuste do exercicio

---
+++ exercicio03/src/exercicio03.java
@@ -0,0 +1,106 @@
+package exercicio03;
+import java.util.ArrayList;
+
+class Barco {
+    private String nome;
+    private int tamanho;
+
+    public Barco(String nome, int tamanho) {
+        this.nome = nome;
+        this.tamanho = tamanho;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " (Tamanho: " + tamanho + ")";
+    }
+}
+
+class BasePorto {
+    private String nome;
+    private ArrayList<Barco> barcosAtracados;
+
+    public BasePorto(String nome) {
+        this.nome = nome;
+        this.barcosAtracados = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void atracarBarco(Barco barco) {
+        barcosAtracados.add(barco);
+        System.out.println(barco.getNome() + " atracado no " + nome);
+    }
+
+    public void desatracarBarco(Barco barco) {
+        barcosAtracados.remove(barco);
+        System.out.println(barco.getNome() + " desatracado do " + nome);
+    }
+
+    public ArrayList<Barco> getBarcosAtracados() {
+        return barcosAtracados;
+    }
+}
+
+class PortoPequeno extends BasePorto {
+    public PortoPequeno(String nome) {
+        super(nome);
+    }
+
+    @Override
+    public void atracarBarco(Barco barco) {
+        if (barco.getTamanho() <= 10) {
+            super.atracarBarco(barco);
+        } else {
+            System.out.println(barco.getNome() + " não pode atracar no " + getNome() + " devido ao tamanho.");
+        }
+    }
+}
+
+class PortoGrande extends BasePorto {
+    public PortoGrande(String nome) {
+        super(nome);
+    }
+
+    @Override
+    public void atracarBarco(Barco barco) {
+        if (barco.getTamanho() >= 10) {
+            super.atracarBarco(barco);
+        } else {
+            System.out.println(barco.getNome() + " não pode atracar no " + getNome() + " devido ao tamanho.");
+        }
+    }
+}
+
+public class exercicio03 {
+    public static void main(String[] args) {
+        Barco barco1 = new Barco("Barco 1", 2);
+        Barco barco2 = new Barco("Barco 2", 5);
+        Barco barco3 = new Barco("Barco 3", 7);
+        Barco barco4 = new Barco("Barco 4", 12);
+        Barco barco5 = new Barco("Barco 5", 20);
+
+        PortoPequeno portoPequeno = new PortoPequeno("Porto Pequeno");
+        PortoGrande portoGrande = new PortoGrande("Porto Grande");
+
+        Barco[] barcos = {barco1, barco2, barco3, barco4, barco5};
+
+        for (Barco barco : barcos) {
+            if (barco.getTamanho() <= 10) {
+                portoPequeno.atracarBarco(barco);
+            } else {
+                portoGrande.atracarBarco(barco);
+            }
+        }
+    }
+}
